f9c778231474d72cbc406e9a3aca723a56d324b6
web-orbit12 <goblogger125@gmail.com>
2023-04-09 01:07:38 +0400

    Basic Operators

---
+++ exoshell5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+COST_PINEAPPLE=50
+
+COST_BANANA=4
+
+COST_WATERMELON=23
+
+COST_BASKET=1
+
+## Note Double parethesis important here
+TOTAL=$((${COST_PINEAPPLE}+(2*${COST_BANANA})+(3*${COST_WATERMELON})+${COST_BASKET}))
+
+echo "Total Cost is $TOTAL"
